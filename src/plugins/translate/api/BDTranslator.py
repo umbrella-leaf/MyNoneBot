0e3db78d5458b9b3b0b6b36166bc8c933173e338
Liu Zhongnuo <2313678365@qq.com>
2023-07-02 19:49:24 +0800

    feat: 添加百度翻译公共API并调整私有API

--- src/plugins/translate/api/BDTranslator.py
+++ src/plugins/translate/api/BDTranslator.py
@@ -3,14 +3,17 @@ import httpx
 from random import randint
 from hashlib import md5
 from urllib import parse
+import execjs
 
 
 class BaiduTranslator:
     def __init__(self, appid: str, secret_key: str):
+        self.base_url = "http://api.fanyi.baidu.com/api/trans/vip"
         self.appid = appid
         self.secret_key = secret_key
         self.salt = ''
         self.sign = ''
+        self.client = httpx.AsyncClient(timeout=True)
 
     def generate_salt(self, num: int):
         self.salt = ''
@@ -33,10 +36,65 @@ class BaiduTranslator:
             "salt": self.salt,
             "sign": self.sign
         }
-        base_url = "http://api.fanyi.baidu.com/api/trans/vip/translate"
         param = parse.urlencode(data)
-        url = f"{base_url}?{param}"
+        url = f"{self.base_url}/translate?{param}"
 
-        client = httpx.AsyncClient(timeout=None)
-        res = await client.get(url=url)
+        res = await self.client.get(url=url)
         return json.loads(res.text)['trans_result'][0]['dst']
+
+
+class BaiduPublicTranslator:
+    def __init__(self, sign_generate_js: str, acs_token: str, cookie: str):
+        self.base_url = "https://fanyi.baidu.com"
+        self.sign_generate_js = sign_generate_js
+        self.acs_token = acs_token
+        self.cookie = cookie
+        self.client = httpx.AsyncClient(timeout=None)
+
+    def generate_sign(self, text: str):
+        sign = execjs.compile(self.sign_generate_js).call("e", text)
+        return sign
+
+    async def lang_detect(self, text: str):
+        url = f"{self.base_url}/langdetect"
+        res = await self.client.post(
+            url=url,
+            data={
+                "query": text
+            }
+        )
+        return json.loads(res.text)["lan"]
+
+    @staticmethod
+    def lang_choice(lan: str):
+        if lan == "zh":
+            return "zh", "en"
+        return "en", "zh"
+
+    async def translate(self, text: str):
+        sign = self.generate_sign(text=text)
+        from_lan, to_lan = self.lang_choice(
+            lan=await self.lang_detect(text=text)
+        )
+        params = {
+            "from": from_lan,
+            "to": to_lan,
+            "query": text,
+            "transtype": "translang",
+            "simple_means_flag": "3",
+            "sign": sign,
+            "token": "2ebec4ea4428f385ad2b8dbaa9102727"
+        }
+        headers = {
+            "Acs-Token": self.acs_token,
+            "Cookie": self.cookie,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/73.0.3683.86 Safari/537.36"
+        }
+        url = f"{self.base_url}/v2transapi"
+        res = await self.client.post(
+            url=url,
+            data=params,
+            headers=headers
+        )
+        return json.loads(res.text)["trans_result"]["data"][0]["dst"]
