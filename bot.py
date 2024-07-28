b91bd524941e7507e418c82624864d9fc3013744
umbrella-leaf <2313678365@qq.com>
2024-07-29 01:50:14 +0800

    feat: 注册qq adapter

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+from nonebot.adapters.qq import Adapter as QQGuild_Adapter
 
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
+driver.register_adapter(QQGuild_Adapter)
 
 nonebot.load_builtin_plugins('echo')
 
