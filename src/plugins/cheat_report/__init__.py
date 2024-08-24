71ff3bce728f834637198b93ae41e9b6921f27c4
umbrella-leaf <2313678365@qq.com>
2024-08-24 23:11:51 +0800

    fix: 修复群文件与表情存档的问题

--- src/plugins/cheat_report/__init__.py
+++ src/plugins/cheat_report/__init__.py
@@ -1,27 +1,35 @@
-from nonebot import on_message
+from nonebot import on_message, on
 from nonebot.adapters.onebot.v11 import (
     Bot as QQBot,
     GroupMessageEvent,
+    GroupUploadNoticeEvent,
     MessageSegment
 )
 from typing import Union
 from .utils.common import upload_report
 
 Bot = Union[QQBot]
-Event = Union[GroupMessageEvent]
+Event = Union[GroupMessageEvent, GroupUploadNoticeEvent]
 
-cheaterReportGroupId = '645832801'
+cheaterReportGroupId = ['645832801']
 
 
 async def is_cheater_record_group(event: Event) -> bool:
-    return str(event.group_id) == cheaterReportGroupId
+    if str(event.group_id) not in cheaterReportGroupId:
+        return False
+    if isinstance(event, GroupMessageEvent):
+        message = str(event.get_message())
+        if message == '':
+            return False
+    return True
 
 
-cheaterRecord = on_message(rule=is_cheater_record_group, priority=18, block=True)
+cheaterRecord = on(rule=is_cheater_record_group, priority=18, block=True)
 
 
 @cheaterRecord.handle()
 async def handle_cheater_report(bot: Bot, event: Event):
     await upload_report(event, bot)
     await cheaterRecord.finish()
+
 
--- src/plugins/cheat_report/utils/common.py
+++ src/plugins/cheat_report/utils/common.py
@@ -1,26 +1,31 @@
 import os
+import re
 import datetime
 import httpx
-from typing import Dict, Any
+from uuid import uuid4
+from typing import Dict, Any, Union
 from nonebot import require
-from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, GroupUploadNoticeEvent
+
+
+Event = Union[GroupMessageEvent, GroupUploadNoticeEvent]
 
 require("QQGuild")
 from src.plugins.QQGuild import dbclient
 
-
 resource_path = "https://report.umbrella-leaf.com"
+temp_group_file_path = "/root/.config/QQ/NapCat/temp"
 image_suffix = ["jpg", "png"]
 video_suffix = ["mp4"]
 resource_types = ["video", "image"]
 
 
-async def get_member_user_info(event: GroupMessageEvent, bot: Bot) -> Dict[str, Any]:
+async def get_member_user_info(event: Event, bot: Bot) -> Dict[str, Any]:
     group_id = event.group_id
     user_id = event.user_id
     user_info = await bot.get_group_member_info(group_id=group_id,
-                                           user_id=user_id,
-                                           no_cache=False)
+                                                user_id=user_id,
+                                                no_cache=False)
     user_info["avatar_url"] = f"http://q1.qlogo.cn/g?b=qq&nk={user_id}&s=640"
     return user_info
 
@@ -33,41 +38,55 @@ async def acquire_resources(name: str, url: str):
         os.makedirs(f"resources/{folder}s", exist_ok=True)
 
     save_path = f"{resource_type}s/{resource_name}"
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(verify=False) as client:
         response = await client.get(url)
         with open(f"resources/{save_path}", "wb") as fw:
             fw.write(response.content)
     return f"{resource_path}/{save_path}"
 
 
-async def extract_appendices(event: GroupMessageEvent, bot: Bot):
+async def extract_appendices(event: Event, bot: Bot):
     mentions = []
     appendices = []
     message = ""
-    raw_message = event.get_message()
-    for segment in raw_message:
-        if segment.type == "at":
-            mention_id = segment.data["qq"]
-            mention_info = await bot.get_group_member_info(group_id=event.group_id,
-                                                           user_id=mention_id,
-                                                           no_cache=False)
-            mention_username = mention_info["nickname"]
-            mentions.append(f"@{mention_username}")
-        elif segment.type in resource_types:
-            resource_url = segment.data["url"]
-            resource_name = segment.data["file"]
-            resource_url = await acquire_resources(resource_name, resource_url)
-            appendices.append(resource_url)
-        elif segment.type == "text":
-            message = segment.data["text"].strip()
-    if message != "":
-        mentions.append(message)
-    message = " ".join(mentions)
+    if isinstance(event, GroupMessageEvent):
+        raw_message = event.get_message()
+        for segment in raw_message:
+            if segment.type == "at":
+                mention_id = segment.data["qq"]
+                mention_info = await bot.get_group_member_info(group_id=event.group_id,
+                                                               user_id=mention_id,
+                                                               no_cache=False)
+                mention_username = mention_info["nickname"]
+                mentions.append(f"@{mention_username}")
+            elif segment.type in resource_types:
+                resource_url = segment.data["url"]
+                resource_name = segment.data.get("filename", None)
+                if resource_name is None:
+                    resource_name = f"{uuid4().hex}.mp4"
+                resource_url = await acquire_resources(resource_name, resource_url)
+                appendices.append(resource_url)
+            elif segment.type == 'face':
+                message += str(segment)
+            elif segment.type == "text":
+                text = segment.data["text"]
+                if re.match(r'^\[.*?]$', text):
+                    continue
+                message += segment.data["text"]
+    else:
+        group_id = event.group_id
+        file_id = event.file.id
+        resource_name = event.file.name
+        busid = event.file.busid
+        resource_url = (await bot.get_group_file_url(group_id=group_id, file_id=file_id, busid=busid))["url"]
+        resource_url = await acquire_resources(resource_name, resource_url)
+        appendices.append(resource_url)
+    message = " ".join(mentions) + message
     appendix_urls = " ".join(appendices)
     return message, appendix_urls
 
 
-async def upload_report(event: GroupMessageEvent, bot: Bot):
+async def upload_report(event: Event, bot: Bot):
     user_info = await get_member_user_info(event, bot)
     nickname = user_info["nickname"]
     avatar_url = user_info["avatar_url"]
@@ -82,4 +101,3 @@ async def upload_report(event: GroupMessageEvent, bot: Bot):
         appendix=appendix_urls,
         send_time=formatted_time
     )
-
