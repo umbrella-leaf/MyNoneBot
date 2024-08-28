ac026fb6f1e7ee876306d0d0e671ee84ef5dec5b
umbrella-leaf <2313678365@qq.com>
2024-08-29 04:14:54 +0800

    feat: 为redis连接器添加对哈希表和列表的支持

--- src/plugins/ai_chat/redis.py
+++ src/plugins/ai_chat/redis.py
@@ -1,4 +1,5 @@
 import redis
+from typing import Any
 from .config import plugin_config
 
 
@@ -23,7 +24,7 @@ class Redis:
         if len(chats):
             redis_conn.delete(*chats)
 
-    def set(self, key: str, value: str, ex:int = 0):
+    def set(self, key: Any, value: Any, ex: int = 0):
         try:
             redis_conn = redis.Redis(connection_pool=self.redis_pool)
             redis_conn.set(key, value, ex=ex)
@@ -34,5 +35,54 @@ class Redis:
         try:
             redis_conn = redis.Redis(connection_pool=self.redis_pool)
             return redis_conn.get(key)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def hset(self, name, key, value):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            redis_conn.hset(name, key, value)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def hget(self, name, key):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            return redis_conn.hget(name, key)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def hgetall(self, name):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            return redis_conn.hgetall(name)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def lpush(self, name, *values):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            redis_conn.lpush(name, *values)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def rpush(self, name, *values):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            redis_conn.rpush(name, *values)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def lrange(self, name, start=0, end=-1):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            return redis_conn.lrange(name, start, end)
+        except redis.exceptions.RedisError as err:
+            print(f'RedisError: {err}')
+
+    def expire(self, name, time):
+        try:
+            redis_conn = redis.Redis(connection_pool=self.redis_pool)
+            redis_conn.expire(name, time)
         except redis.exceptions.RedisError as err:
             print(f'RedisError: {err}')
